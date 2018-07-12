2e6c21e694050f3214112291d2e24162757dac72
Stefan Adamov <stefan.adamof@gmail.com>
2018-07-12 16:03:57 +0300

    Adds deep_merge Ruby implementation

---
+++ deep_merge.rb
@@ -0,0 +1,23 @@
+class Hash
+  def deep_merge(other_hash, &block)
+    dup.deep_merge!(other_hash, &block)
+  end
+
+  def deep_merge!(other_hash, &block)
+    other_hash.each_pair do |current_key, other_value|
+      this_value = self[current_key]
+
+      self[current_key] = if this_value.is_a?(Hash) && other_value.is_a?(Hash)
+        this_value.deep_merge(other_value, &block)
+      else
+        if block_given? && key?(current_key)
+          block.call(current_key, this_value, other_value)
+        else
+          other_value
+        end
+      end
+    end
+
+    self
+  end
+end
